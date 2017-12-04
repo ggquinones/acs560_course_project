c961c0dd56a75ef4be8aaa8282ea889db21e7faf
Gabriel Quinones <quingg01@students.ipfw.edu>
2017-12-04 17:43:56 -0500

    Final handles added

--- GutenLib/BackEnd/src/gutenlib/clientproxy/scripts.sql
+++
@@ -1,9 +0,0 @@
-
-
-
-CREATE TABLE userbooks(
-userid integer references users(userid) ON DELETE CASCADE,
-bookid integer references books(bookid) ON DELETE CASCADE,
-lastread text,
-constraint pk_userbooks primary key(userid,bookid)
-);
